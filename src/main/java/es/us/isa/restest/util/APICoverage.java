ce8134bc397498f664a28af816a887f64decae09
nessq-demo <36137485+nessq-demo@users.noreply.github.com>
2021-12-16 15:53:08 +0530

    Update APICoverage.java

--- src/main/java/es/us/isa/restest/util/APICoverage.java
+++ src/main/java/es/us/isa/restest/util/APICoverage.java
@@ -1,4 +1,51 @@
 package es.us.isa.restest.util;
 
-public class APICoverage {
+import java.util.ArrayList;
+
+public class APICoverage 
+{
+	private String apiName;
+	private ArrayList<FieldCoverage> fieldCoverages;
+	private String methodName;
+	private int actualTestCaseCount;
+	private int expectedTestCaseCount;
+	private boolean isApiFoundInExecution = false;
+
+	public boolean isApiFoundInExecution() {
+		return isApiFoundInExecution;
+	}
+	public void setApiFoundInExecution(boolean isApiFoundInExecution) {
+		this.isApiFoundInExecution = isApiFoundInExecution;
+	}
+	public int getActualTestCaseCount() {
+		return actualTestCaseCount;
+	}
+	public void setActualTestCaseCount(int actualTestCaseCount) {
+		this.actualTestCaseCount = actualTestCaseCount;
+	}
+	public int getExpectedTestCaseCount() {
+		return expectedTestCaseCount;
+	}
+	public void setExpectedTestCaseCount(int expectedTestCaseCount) {
+		this.expectedTestCaseCount = expectedTestCaseCount;
+	}
+	public ArrayList<FieldCoverage> getFieldCoverages() {
+		return fieldCoverages;
+	}
+	public void setFieldCoverages(ArrayList<FieldCoverage> fieldCoverages) {
+		this.fieldCoverages = fieldCoverages;
+	}
+	public String getMethodName() {
+		return methodName;
+	}
+	public void setMethodName(String methodName) {
+		this.methodName = methodName;
+	}
+	public String getApiName() {
+		return apiName;
+	}
+	public void setApiName(String apiName) {
+		this.apiName = apiName;
+	}
+
 }
