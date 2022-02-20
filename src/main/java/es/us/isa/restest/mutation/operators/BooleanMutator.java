9321dc775d3be0f83866ab1179e979971afc3349
nessq-demo <36137485+nessq-demo@users.noreply.github.com>
2022-02-20 21:51:32 +0530

    Create BooleanMutator.java

---
+++ src/main/java/es/us/isa/restest/mutation/operators/BooleanMutator.java
@@ -0,0 +1,26 @@
+package es.us.isa.restest.mutation.operators;
+
+import es.us.isa.jsonmutator.mutator.AbstractMutator;
+import es.us.isa.jsonmutator.mutator.value.boolean0.operator.BooleanMutationOperator;
+import es.us.isa.jsonmutator.mutator.value.common.operator.ChangeTypeOperator;
+import es.us.isa.jsonmutator.mutator.value.common.operator.NullOperator;
+import es.us.isa.jsonmutator.util.OperatorNames;
+
+import static es.us.isa.jsonmutator.util.PropertyManager.readProperty;
+
+/**
+ * Given a set of boolean mutation operators, the BooleanMutator selects one based
+ * on their weights and returns the mutated boolean.
+ *
+ * @author Alberto Martin-Lopez
+ */
+public class BooleanMutator extends AbstractMutatorLocal {
+
+    public BooleanMutator() {
+        super();
+        prob = Float.parseFloat(readProperty("operator.value.boolean.prob"));
+        operators.put(OperatorNames.MUTATE, new BooleanMutationOperator());
+        operators.put(OperatorNames.NULL, new NullOperator(Boolean.class));
+        operators.put(OperatorNames.CHANGE_TYPE, new ChangeTypeOperator(Boolean.class));
+    }
+}
