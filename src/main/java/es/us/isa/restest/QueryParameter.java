c5ee427b215dcef03a7abfa4ee45e5b77a7bb336
nessq-demo <36137485+nessq-demo@users.noreply.github.com>
2022-02-20 21:12:16 +0530

    Create QueryParameter.java

---
+++ src/main/java/es/us/isa/restest/QueryParameter.java
@@ -0,0 +1,24 @@
+package es.us.isa.restest.apichain.api.util;
+
+public class QueryParameter {
+
+	private String queryParameterName;
+	private String queryParameterValue;
+	
+	public String getQueryParameterName() {
+		return queryParameterName;
+	}
+	
+	public void setQueryParameterName(String queryParameterName) {
+		this.queryParameterName = queryParameterName;
+	}
+	
+	public String getQueryParameterValue() {
+		return queryParameterValue;
+	}
+	
+	public void setQueryParameterValue(String queryParameterValue) {
+		this.queryParameterValue = queryParameterValue;
+	}
+	
+}
