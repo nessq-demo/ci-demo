c6d4159f5af529c82091f1fb2d01be9b44ae9b76
nessq-demo <36137485+nessq-demo@users.noreply.github.com>
2022-02-20 22:02:50 +0530

    Update RESTestRunner.java

--- src/main/java/es/us/isa/restest/runners/RESTestRunner.java
+++ src/main/java/es/us/isa/restest/runners/RESTestRunner.java
@@ -1,12 +1,20 @@
 package es.us.isa.restest.runners;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Iterator;
+import java.util.List;
 
-import es.us.isa.restest.specification.OpenAPISpecification;
 import es.us.isa.restest.util.*;
 import es.us.isa.restest.util.ClassLoader;
+
+import org.apache.commons.io.FileUtils;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.junit.runner.JUnitCore;
 import org.junit.runner.Result;
 
@@ -16,6 +24,8 @@ import es.us.isa.restest.reporting.StatsReportManager;
 import es.us.isa.restest.testcases.TestCase;
 import es.us.isa.restest.testcases.writers.IWriter;
 
+import static es.us.isa.restest.util.FileManager.checkIfExists;
+import static es.us.isa.restest.util.FileManager.getFileNames;
 import static es.us.isa.restest.util.Timer.TestStep.*;
 
 /**
@@ -33,18 +43,12 @@ public class RESTestRunner {
 	protected IWriter writer;							// RESTAssured writer
 	protected AllureReportManager allureReportManager;	// Allure report manager
 	protected StatsReportManager statsReportManager;	// Stats report manager
-	private boolean executeTestCases = true;			// Whether to execute test cases
-	private boolean allureReports = true;				// Whether to actually generate reports or not (folder "allure-reports")
+	private boolean executeTestCases;
 	private int numTestCases = 0;						// Number of test cases generated so far
-
-	private boolean learnRegex;
-	private boolean secondPredicateSearch;
-	private OpenAPISpecification spec;
-	private String confPath;
-
 	private static final Logger logger = LogManager.getLogger(RESTestRunner.class.getName());
 
-	public RESTestRunner(String testClassName, String targetDir, String packageName, Boolean learnRegex, Boolean secondPredicateSearch, OpenAPISpecification spec, String confPath, AbstractTestCaseGenerator generator, IWriter writer, AllureReportManager reportManager, StatsReportManager statsReportManager) {
+	public RESTestRunner(String testClassName, String targetDir, String packageName, AbstractTestCaseGenerator generator, IWriter writer,
+						 AllureReportManager reportManager, StatsReportManager statsReportManager) {
 		this.targetDir = targetDir;
 		this.packageName = packageName;
 		this.testClassName = testClassName;
@@ -52,14 +56,8 @@ public class RESTestRunner {
 		this.writer = writer;
 		this.allureReportManager = reportManager;
 		this.statsReportManager = statsReportManager;
-
-		this.learnRegex = learnRegex;
-		this.secondPredicateSearch = secondPredicateSearch;
-		this.spec = spec;
-		this.confPath = confPath;
-
 	}
-	  
+
 	public void run() throws RESTestException {
 
 		// Test generation and writing (RESTAssured)
@@ -73,22 +71,125 @@ public class RESTestRunner {
 		}
 
 		generateReports();
-
-		if(learnRegex){
-			statsReportManager.learn(testId, spec, confPath);
+	}
+	
+	private ArrayList<String> getEnabledReleases(String releaseDirectory)
+	{
+		
+		String file = releaseDirectory+"/releases.xlsx";
+		String sheetName = "releases";
+		
+		ArrayList<String> releases = new ArrayList<>();
+		
+		try 
+		{
+			boolean flag=checkIfExists(file);
+			if(!flag) 
+			{
+				return releases;
+			}
+			
+			Xls_Reader xls = new Xls_Reader(file);
+			for(int i=2;i<=xls.getRowCount(sheetName);i++)
+			{
+				if(xls.getCellData(sheetName,"Execute",i).equalsIgnoreCase("N"))
+				{
+					releases.add(xls.getCellData(sheetName,"ReleaseName",i));
+				}
+			}	
+			
+		} catch (Exception e) {
+			e.printStackTrace();
 		}
+		return releases;
 	}
 
+	private void copyReleaseTestFiles(String releaseDirectory, String testFilePath) throws Exception
+	{
+		ArrayList<String> releases = getEnabledReleases(releaseDirectory);
+		File releaseDir = new File(releaseDirectory);
+		File tempReleaseDir = new File(releaseDirectory+"/temp");
+		FileUtils.copyDirectory(releaseDir, tempReleaseDir);
+		File relDirTemp; 
+		
+		for(String releaseName : releases)
+		{
+			relDirTemp = new File(releaseDirectory+"/temp/"+releaseName);
+			if(relDirTemp.exists() && relDirTemp.isDirectory())
+			{
+				FileUtils.deleteDirectory(relDirTemp);
+			}
+		}
+		String[] extensions = new String[] {"java"};
+		Collection<File> files = FileUtils.listFiles( tempReleaseDir , extensions, true);
+		for (File file : files) 
+		{
+            FileUtils.copyFile(file, new File(testFilePath+"/"+file.getName()));
+		}
+		FileUtils.deleteDirectory(tempReleaseDir);
+		
+		
+	}
+
+	
+	public void execute() throws RESTestException, Exception {
+
+		String testFilePath = targetDir+"/Test";
+		
+		if(executeTestCases) 
+		{
+			
+			if(ReleaseManager.isExecuteReleaseCases())
+			{
+				String releaseDirectory = targetDir+"/releases";
+				File releaseDir = new File(releaseDirectory);
+				
+				if(releaseDir.exists())
+				{	
+					copyReleaseTestFiles(releaseDirectory, testFilePath);
+					
+				}
+			}
+				
+			// Test execution
+			logger.info("Running tests");
+			System.setProperty("allure.results.directory", allureReportManager.getResultsDirPath());
+			//String testFilePath = targetDir;
+			List<File> testFileList = getFileNames(testFilePath);
+			
+			if(testFileList.size() ==0)
+			{
+				logger.info("===========================================================");
+				logger.info("   No Test Class in '"+testFilePath+"' available to Run    ");
+				logger.info("===========================================================");
+				System.exit(-1);
+				
+			}
+			for(int i=0;i<testFileList.size();i++) {
+				testExecution(getTestExecutionClass(testFileList.get(i)));
+			}
+		}
+		generateReports();
+		
+		logger.info("Test Completed.Cleaning Directory'"+testFilePath+"' ");
+		FileUtils.cleanDirectory(new File(testFilePath));
+	}
+	public void generateTest() throws RESTestException {
+		// Test generation and writing (RESTAssured)
+		testGeneration();
+	}
+
+	
 	protected void generateReports() {
-		if(executeTestCases && allureReports) {
+		if(executeTestCases) {
 			// Generate test report
 			logger.info("Generating test report");
 			allureReportManager.generateReport();
 		}
 
 		// Generate coverage report
-		logger.info("Generating CSV data");
-		statsReportManager.generateReport(testId, executeTestCases);
+		logger.info("Generating coverage report");
+		//statsReportManager.generateReport(testId, executeTestCases);
 	}
 
 	protected Class<?> getTestClass() {
@@ -96,6 +197,20 @@ public class RESTestRunner {
 		String filePath = targetDir + "/" + testClassName + ".java";
 		String className = packageName + "." + testClassName;
 		logger.info("Compiling and loading test class {}.java", className);
+		return ClassLoader.loadClass(filePath, className);
+	}
+
+	protected Class<?> getTestExecutionClass(File file) {
+		// Load test class
+		String filePath = file.getPath();
+		//System.out.println("filePath ->"+filePath);
+		String testFileName = file.getName();
+		//System.out.println("testFileName ->"+testFileName);
+		String classNameGenerated = file.getName().substring(0,testFileName.length()-5);
+		String className = packageName + "." + classNameGenerated;
+		logger.info("Compiling and loading test class {}.java", className);
+		//System.out.println("classNameGenerated ->"+classNameGenerated);
+		//System.out.println("className ->"+className);
 		return ClassLoader.loadClass(filePath, className);
 	}
 
@@ -117,18 +232,32 @@ public class RESTestRunner {
         writer.write(testCases);
 
 	}
+	
+   public String getFilePath() throws RESTestException {
+        String filePath = targetDir + "/" + testClassName + ".java";
+    return filePath;
+	}
 
 	protected void testExecution(Class<?> testClass)  {
-		
-		JUnitCore junit = new JUnitCore();
-		//junit.addListener(new TextListener(System.out));
-		junit.addListener(new io.qameta.allure.junit4.AllureJunit4());
-		Timer.startCounting(TEST_SUITE_EXECUTION);
-		Result result = junit.run(testClass);
-		Timer.stopCounting(TEST_SUITE_EXECUTION);
-		int successfulTests = result.getRunCount() - result.getFailureCount() - result.getIgnoreCount();
-		logger.info("{} tests run in {} seconds. Successful: {}, Failures: {}, Ignored: {}", result.getRunCount(), result.getRunTime()/1000, successfulTests, result.getFailureCount(), result.getIgnoreCount());
-
+		DBConnector db = new DBConnector();
+		try {
+			db.initConnection(true);
+			JUnitCore junit = new JUnitCore();
+			//junit.addListener(new TextListener(System.out));
+			junit.addListener(new io.qameta.allure.junit4.AllureJunit4());
+			Timer.startCounting(TEST_SUITE_EXECUTION);
+			Result result = junit.run(testClass);
+			Timer.stopCounting(TEST_SUITE_EXECUTION);
+			int successfulTests = result.getRunCount() - result.getFailureCount() - result.getIgnoreCount();
+			logger.info("{} tests run in {} seconds. Successful: {}, Failures: {}, Ignored: {}", result.getRunCount(), result.getRunTime() / 1000, successfulTests, result.getFailureCount(), result.getIgnoreCount());
+		    String className = String.valueOf(testClass).replace("class zycus_new.","").trim();
+			db.insertReportTable(result.getRunCount(),successfulTests,result.getFailureCount(), className);
+		}catch(Throwable t){
+			t.printStackTrace();
+		}finally {
+			db.closeConnection();
+			db=null;
+		}
 	}
 	
 	public String getTargetDir() {
@@ -161,9 +290,5 @@ public class RESTestRunner {
 
 	public void setExecuteTestCases(Boolean executeTestCases) {
 		this.executeTestCases = executeTestCases;
-	}
-
-	public void setAllureReport(boolean allureReports) {
-		this.allureReports = allureReports;
 	}
 }
