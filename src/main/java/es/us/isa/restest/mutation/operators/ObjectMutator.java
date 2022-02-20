b498172efcde6d5a304e29546aa97209db439dcd
nessq-demo <36137485+nessq-demo@users.noreply.github.com>
2022-02-20 21:54:58 +0530

    Create ObjectMutator.java

---
+++ src/main/java/es/us/isa/restest/mutation/operators/ObjectMutator.java
@@ -0,0 +1,37 @@
+package es.us.isa.restest.mutation.operators;
+
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import es.us.isa.jsonmutator.mutator.AbstractObjectOrArrayMutator;
+import es.us.isa.jsonmutator.mutator.object.operator.ObjectAddElementOperator;
+import es.us.isa.jsonmutator.mutator.object.operator.ObjectRemoveElementOperator;
+import es.us.isa.jsonmutator.mutator.object.operator.ObjectRemoveObjectTypeElementOperator;
+import es.us.isa.jsonmutator.mutator.value.common.operator.ChangeTypeOperator;
+import es.us.isa.jsonmutator.mutator.value.common.operator.NullOperator;
+import es.us.isa.jsonmutator.util.OperatorNames;
+
+/**
+ * Given a set of object mutation operators, the ObjectMutator selects one based
+ * on their weights and returns the mutated object.
+ *
+ * @author Alberto Martin-Lopez
+ */
+public class ObjectMutator extends AbstractObjectOrArrayMutatorLocal {
+
+    public ObjectMutator() {
+        super();
+    }
+
+    public void resetOperators() {
+        operators.clear();
+        operators.put(OperatorNames.REMOVE_ELEMENT, new ObjectRemoveElementOperator());
+        operators.put(OperatorNames.REMOVE_OBJECT_ELEMENT, new ObjectRemoveObjectTypeElementOperator());
+        operators.put(OperatorNames.ADD_ELEMENT, new ObjectAddElementOperator());
+        operators.put(OperatorNames.NULL, new NullOperator(ObjectNode.class));
+        operators.put(OperatorNames.CHANGE_TYPE, new ChangeTypeOperator(ObjectNode.class));
+    }
+
+    public void resetFirstLevelOperators() {
+        operators.remove(OperatorNames.NULL);
+        operators.remove(OperatorNames.CHANGE_TYPE);
+    }
+}
